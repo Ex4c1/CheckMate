4086db7c49785b853dd645035f117683dbce8c9d
Ex4c1 <91961315+Ex4c1@users.noreply.github.com>
2022-03-07 21:58:25 +0100

    Update lol.py

--- lol.py
+++ lol.py
@@ -3,6 +3,8 @@ __AUTHORS__ = "Ex4c && NICEA"
 
 import requests
 
+
+#===============| Basic Vars |===============#
 url = "https://insult.mattbas.org/api/"
 key = ""
 token_ssl = False
@@ -12,13 +14,20 @@ def obtain_request(url,key,token_ssl):
   pass = input("Provide password(default:NONE")
   requests.get(url,auth=($user, $pass) token_ssl)
 
-api_request_reponse = obtain_request(url,key,token_ssl)
-
 def create_menu():
   print('------------------')
   print('|INSULT GENERATOR|')
   print('------------------')
   print('Choose Option:')
   print('1. INSULT ME.\n2. INSULT ME LIKE BIATCH\n3. Exit:')
+  user_menu_input = input('\n\m:>')
+  return user_menu_input
   
-  
+def generate_insult(user_input,level_of_insult):
+  level_of_insult= level_of_insult*user_input
+  for x in level_of_insult:
+    
+ 
+
+#===============| MAIN |===============#
+api_request_reponse = obtain_request(url,key,token_ssl)
