0e7799f675c448dd0ee74ec31ccef66ef4ce4cb7
Ex4c1 <91961315+Ex4c1@users.noreply.github.com>
2022-03-07 21:44:28 +0100

    Update lol.py

--- lol.py
+++ lol.py
@@ -6,7 +6,7 @@ token_ssl = False
 
 def obtain_request(url,key,token_ssl):
 user = input("Provide username(default:NONE")
-pass = user = input("Provide password(default:NONE")
+pass = input("Provide password(default:NONE")
 requests.get(url,auth=('user', 'pass') token_ssl)
 
 api_request_reponse = obtain_request(url,key,token_ssl)
