30d4752ea08d55241f6aad52c02a75371766285d
Ex4c1 <91961315+Ex4c1@users.noreply.github.com>
2022-03-07 21:46:46 +0100

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,12 +1,16 @@
 import requests
 
-url = ""
+url = "https://insult.mattbas.org/api/"
 key = ""
 token_ssl = False
 
 def obtain_request(url,key,token_ssl):
-user = input("Provide username(default:NONE")
-pass = input("Provide password(default:NONE")
-requests.get(url,auth=('user', 'pass') token_ssl)
+  user = input("Provide username(default:NONE")
+  pass = input("Provide password(default:NONE")
+  requests.get(url,auth=($user, $pass) token_ssl)
 
 api_request_reponse = obtain_request(url,key,token_ssl)
+
+def create_menu():
+  
+  
